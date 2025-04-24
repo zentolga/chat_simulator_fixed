69a272e91ad08c8d7b0274c31b06bda4c4db4529
zentolga <zentolga@gmail.com>
2025-04-25 00:43:57 +0200

    Hazır zip'ten proje yüklendi

--- lib/utils/premium.dart
+++ lib/utils/premium.dart
@@ -1,14 +1,1 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class PremiumManager {
-  static Future<bool> isPremium() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('premium') ?? false;
-  }
-
-  static Future<void> activatePremium() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('premium', true);
-    await prefs.setInt('tokens', 9999);
-  }
-}
+// premium.dart placeholder content
